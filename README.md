d28fcd0f5f636b1b00f379065bfa7754e46dcc2d
Sandro Keil <github@sandro-keil.de>
2021-10-08 15:16:06 +0200

    Add setup script

---
+++ README.md
@@ -0,0 +1,13 @@
+# Cody Typescript Tutorial
+
+Please clone this repository and execute the `setup.sh` script to speed up your [Cody PHP Tutotial setup](https://wiki.prooph-board.com/PHP-Cody-Tutorial).
+
+For a quick start execute the commands beneath:
+
+```
+git clone git@github.com:proophboard/php-cody-tutorial.git
+cd php-cody-tutorial
+./setup.sh
+```
+
+Open the [free version of prooph board](https://free.prooph-board.com/i) and head over for your [first exercise from the Wiki](https://wiki.prooph-board.com/PHP-Cody-Tutorial-Exercise-I).
---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/event-engine/php-iio-cody-tutorial.git exercises
+
+cd exercises
+docker-compose run --rm composer install
+
+cd ..
+
+# Install Cody Server
+git clone https://github.com/event-engine/php-inspectio-cody.git cody-bot
+cd cody-bot
+cp .env.dist .env # Adjust UID in .env if needed
+cp app.env.dist app.env
+
+
+sed -i '/app$/a\      - ../exercises:/exercises' docker-compose.yml
+sed -i "14 i \$context->path = '/exercises/src';" codyconfig.php
+
+docker run --rm -it \
+    -v $(pwd):/app \
+    prooph/composer:8.0 install
+
+./dev.sh
+
+echo ""
+echo "Cody tutorial ready. Visit https://wiki.prooph-board.com/PHP-Cody-Tutorial-Exercise-I"
